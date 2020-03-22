9d021d5c43372488ecb7d806f0da4bd1fa552bff
Jarosław Bagnicki <jarek@bagnicki.net>
2020-03-22 20:37:51 +0100

    Add FluentAsserion to GradeBook.Tests project

--- GradeBook.Tests/BookTests.cs
+++ GradeBook.Tests/BookTests.cs
@@ -1,5 +1,5 @@
-using System;
 using Xunit;
+using FluentAssertions;
 
 namespace GradeBook.Tests
 {
@@ -16,6 +16,9 @@ namespace GradeBook.Tests
             Assert.Equal(1, book.MinGrade);
             Assert.Equal(5, book.MaxGrade);
             Assert.Equal(((1+3+5)/3), book.AvgGrade);
+            book.MinGrade.Should().Be(1);
+            book.MaxGrade.Should().Be(5);
+            book.AvgGrade.Should().Be(((1+3+5)/3));
         }
 
         [Fact]
@@ -32,7 +35,7 @@ namespace GradeBook.Tests
                 MaxGrade = 5,
             };
 
-            Assert.Equal<Stats>(stats, book.GetStats());
+           book.GetStats().Should().BeEquivalentTo(stats);
         }
     }
 }
