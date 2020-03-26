108af7e4b5ada434cf79b3f9bd4cff0d2f1cb8db
Jarosław Bagnicki <jarek@bagnicki.net>
2020-03-26 05:10:51 +0100

    Add GetBoolInput method

--- GradeBook/Program.cs
+++ GradeBook/Program.cs
@@ -1,20 +1,73 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
 
 namespace GradeBook
 {
     class Program
     {
+        static List<Book> Books = new List<Book>();
         static void Main(string[] args)
         {
             CultureInfo.CurrentCulture = new CultureInfo("");
+
             
-            var book = new Book("Math");
+            Console.WriteLine("Hello in GradeBook App \n");
 
-            book.AddGrade(6);
-            book.AddGrade(2);
-            book.AddGrade(3.5);
+            if (Books.Count != 0) {
+                // TODO List list of Books
+            }
+            else {
+                Console.WriteLine("You don't have any books currently.");
+            }
 
-            book.ShowStats();
+            while (true)
+            {
+                Console.Write("Do you want create new book (y/n): ");
+                if(GetBootInput())
+                {
+                    Console.WriteLine("Yes");
+                }
+                else
+                {
+                    Console.WriteLine("No");
+                }
+       
+            }
+        //     var book = new Book("Math");
+
+        //     book.AddGrade(6);
+        //     book.AddGrade(2);
+        //     book.AddGrade(3.5);
+
+        //     book.ShowStats();
+        }
+
+        static bool GetBootInput()
+        {
+            char input = '\0';
+            while(true)
+            {
+                try
+                {
+                    input = Console.ReadKey(true).KeyChar;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+                switch(input)
+                {
+                    case 'y':
+                    case 'Y':
+                        Console.Write($"{input}\n");
+                        return true;
+                    case 'n':
+                    case 'N':
+                        Console.Write($"{input}\n");
+                        return false;
+                }
+            }
         }
     }
 }
